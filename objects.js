b04ab7fa8b6b941a38349f0b4a4bdc260600deb6
Yoon Ho (Tony) Lee <tonylee1993@gmail.com>
2015-03-04 21:34:04 -0800

    color change

--- objects.js
+++ objects.js
@@ -168,16 +168,16 @@ SpriteMorph.prototype.categories =
     ];
 
 SpriteMorph.prototype.blockColor = {
-    motion : new Color(74, 108, 212),
-    looks : new Color(143, 86, 227),
-    sound : new Color(207, 74, 217),
-    pen : new Color(0, 161, 120),
-    control : new Color(230, 168, 34),
-    sensing : new Color(4, 148, 220),
-    operators : new Color(98, 194, 19),
-    variables : new Color(243, 118, 29),
-    lists : new Color(217, 77, 17),
-    other: new Color(150, 150, 150)
+    motion : new Color(205, 205, 150),
+    looks : new Color(0, 0, 0),
+    sound : new Color(205, 205, 150),
+    pen : new Color(0, 0, 0),
+    control : new Color(205, 205, 150),
+    sensing : new Color(0, 0, 0),
+    operators : new Color(205, 205, 150),
+    variables : new Color(0, 0, 0),
+    lists : new Color(205, 205, 150),
+    other: new Color(0, 0, 0)
 };
 
 SpriteMorph.prototype.paletteColor = new Color(55, 55, 55);
