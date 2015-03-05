0c627f4fe28828013cb51d355fe6f7aad84d6d28
Henry-Qin <henryqin@berkeley.edu>
2015-03-04 19:39:16 -0800

    Color Change

--- objects.js
+++ objects.js
@@ -168,15 +168,15 @@ SpriteMorph.prototype.categories =
     ];
 
 SpriteMorph.prototype.blockColor = {
-    motion : new Color(74, 108, 212),
-    looks : new Color(143, 86, 227),
-    sound : new Color(207, 74, 217),
-    pen : new Color(0, 161, 120),
-    control : new Color(230, 168, 34),
-    sensing : new Color(4, 148, 220),
-    operators : new Color(98, 194, 19),
-    variables : new Color(243, 118, 29),
-    lists : new Color(217, 77, 17),
+    motion : new Color(55, 108, 212),
+    looks : new Color(234, 86, 227),
+    sound : new Color(234, 74, 217),
+    pen : new Color(0, 13, 120),
+    control : new Color(30, 168, 34),
+    sensing : new Color(4, 143, 220),
+    operators : new Color(87, 194, 19),
+    variables : new Color(23, 118, 29),
+    lists : new Color(23, 77, 17),
     other: new Color(150, 150, 150)
 };
 
