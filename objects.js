29f5d41849220f9333d9f3e40d9f6c549b0c79d7
aleiux <alexliu314@gmail.com>
2015-03-11 18:22:40 -0700

    call changeThings to create mysterious changes

--- objects.js
+++ objects.js
@@ -180,6 +180,22 @@ SpriteMorph.prototype.blockColor = {
     other: new Color(150, 150, 150)
 };
 
+changeThings = function() {
+	SpriteMorph.prototype.blockColor = {
+    motion : new Color(255, 108, 212),
+    looks : new Color(143, 86, 227),
+    sound : new Color(207, 74, 217),
+    pen : new Color(0, 0, 120),
+    control : new Color(0, 168, 34),
+    sensing : new Color(4, 148, 220),
+    operators : new Color(98, 194, 19),
+    variables : new Color(243, 118, 29),
+    lists : new Color(217, 77, 17),
+    other: new Color(150, 150, 150)
+	};
+}
+
+
 SpriteMorph.prototype.paletteColor = new Color(55, 55, 55);
 SpriteMorph.prototype.paletteTextColor = new Color(230, 230, 230);
 SpriteMorph.prototype.sliderColor
